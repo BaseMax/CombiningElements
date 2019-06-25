21fd196260eba04bb2e7255602610104c87e5ef1
Max Base <MaxBaseCode@Gmail.Com>
2019-06-25 21:45:44 +0430

    Update without-repetition.cpp

--- without-repetition.cpp
+++ without-repetition.cpp
@@ -14,11 +14,11 @@ using namespace std;
 
 int main() {
 	const int length=6;
+	string values[length];
 	cout << "Enter values:\n";
-	string texts[length];
 	for(int index = 0; index < length;index++) {
-		cin >> texts[index];			// Get a word
-		// getline(cin, texts[i]);		// Get a line
+		cin >> values[index];			// Get a word
+		// getline(cin, values[i]);		// Get a line
 	}
 	for(int index1 = 0;index1 < length; index1++) {
 		for(int index2 = 0;index2 < length; index2++) {
@@ -26,7 +26,7 @@ int main() {
 				for(int index4 = 0;index4 < length;index4++) {
 					for(int index5 = 0; index5 < length;index5++) {
 						for(int index6 = 0; index6 < length;index6++) {
-							cout << texts[index1] << texts[index2] << texts[index3] << texts[index4] << texts[index5] << texts[index6] << endl;
+							cout << values[index1] << values[index2] << values[index3] << values[index4] << values[index5] << values[index6] << endl;
 						}
 					}
 				}
