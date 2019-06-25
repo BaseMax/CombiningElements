6954c19ec59b28663e52164fcf71d097c7609f7b
Max Base <MaxBaseCode@Gmail.Com>
2019-06-25 21:45:30 +0430

    Update with-repetition.cpp

--- with-repetition.cpp
+++ with-repetition.cpp
@@ -8,3 +8,62 @@
 * @Repository : https://github.com/BaseMax/CombiningElements
 *
 **/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string values[length];
+	const int length=6;
+	cout << "Enter values:\n";
+	for(int index = 0; index < length;index++) {
+		cin >> values[index];			// Get a word
+		// getline(cin, values[i]);		// Get a line
+	}
+	for(int index1 = 0;index1 < length; index1++) {
+		for(int index2 = 0;index2 < length; index2++) {
+			if(index2 == index1)
+				continue;
+			for(int index3 = 0;index3 < length; index3++) {
+				if(index3 == index1)
+					continue;
+				if(index3 == index2)
+					continue;
+				for(int index4 = 0;index4 < length;index4++) {
+					if(index4 == index1)
+						continue;
+					if(index4 == index2)
+						continue;
+					if(index4 == index3)
+						continue;
+					for(int index5 = 0; index5 < length;index5++) {
+						if(index5 == index1)
+							continue;
+						if(index5 == index2)
+							continue;
+						if(index5 == index3)
+							continue;
+						if(index5 == index4)
+							continue;
+						for(int index6 = 0; index6 < length;index6++) {
+							if(index6 == index1)
+								continue;
+							if(index6 == index2)
+								continue;
+							if(index6 == index3)
+								continue;
+							if(index6 == index4)
+								continue;
+							if(index6 == index5)
+								continue;
+							cout << values[index1] << values[index2] << values[index3] << values[index4] << values[index5] << values[index6] << endl;
+						}
+					}
+				}
+			}
+		}
+	}
+	cin.get();
+	cin.get();
+	return 0;
+}
